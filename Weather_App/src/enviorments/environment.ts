af2582bec1fce78d7dc8e55335df1ad49f78c8e8
saurabh <saurabh15094@gmail.com>
2023-03-31 00:32:59 +0530

    weather app

---
+++ Weather_App/src/enviorments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    weatherApiBaseUrl: 'https://open-weather-map27.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather-map27.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '92cbda81camsh53b590ab58328c1p1ca18djsned036434ac03'
+}
